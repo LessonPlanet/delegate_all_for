868124aac749a8cdf935f59fc66c9516fc2aa72f
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2012-05-02 17:10:45 -0700

    Add ActiveRecord spec support file which sets up our in memory database

---
+++ spec/support/active_record.rb
@@ -0,0 +1,13 @@
+ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: ':memory:'
+ActiveRecord::Migration.create_table :parents do |t|
+  t.string :one
+  t.string :two
+  t.timestamps
+end
+ActiveRecord::Migration.create_table :children do |t|
+  t.string :two
+  t.string :three
+  t.string :four
+  t.integer :parent_id
+  t.timestamps
+end
