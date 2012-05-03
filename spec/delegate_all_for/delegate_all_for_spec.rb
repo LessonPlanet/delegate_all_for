920f486935afeb28f0ce364997746a0891c2848a
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2012-05-02 17:11:19 -0700

    Rearrange Child and Parent definitions so Child is in scope when we delegate to it from Parent

--- spec/delegate_all_for/delegate_all_for_spec.rb
+++ spec/delegate_all_for/delegate_all_for_spec.rb
@@ -1,20 +1,21 @@
 require File.expand_path('../../spec_helper', __FILE__)
+require File.expand_path('../../support/active_record', __FILE__)
+
+class Child < ActiveRecord::Base
+  belongs_to :parent
+  #def self.column_names; %w(two three four) end
+  def two; 'child' end
+  def extra; true end
+end
 
 class Parent < ActiveRecord::Base
-  def self.column_names; %w(one two) end
+  #def self.column_names; %w(one two) end
 
   has_one :child
   delegate_all_for :child, except: [:three], also_include: [:extra]
 
   def initialize; end
   def two; 'parent' end
-end
-
-class Child < ActiveRecord::Base
-  belongs_to :parent
-  def self.column_names; %w(two three four) end
-  def two; 'child' end
-  def extra; true end
 end
 
 describe DelegateAllFor do
