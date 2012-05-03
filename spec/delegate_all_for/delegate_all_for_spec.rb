528514261ed54aef00d98ab3e821c3a2047d28f7
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2012-05-02 17:32:00 -0700

    We're all green!

--- spec/delegate_all_for/delegate_all_for_spec.rb
+++ spec/delegate_all_for/delegate_all_for_spec.rb
@@ -3,30 +3,39 @@ require File.expand_path('../../support/active_record', __FILE__)
 
 class Child < ActiveRecord::Base
   belongs_to :parent
-  #def self.column_names; %w(two three four) end
   def two; 'child' end
   def extra; true end
 end
 
 class Parent < ActiveRecord::Base
-  #def self.column_names; %w(one two) end
-
   has_one :child
   delegate_all_for :child, except: [:three], also_include: [:extra]
-
-  def initialize; end
   def two; 'parent' end
 end
 
+
 describe DelegateAllFor do
   describe 'delegate_all_for' do
-    subject { Parent.new }
+    subject do
+      Parent.create(one: 1) do |p|
+        p.two = 2
+        p.child = Child.create(two: 'two', three: 'three', four: 'four')
+      end
+    end
 
-    it { should respond_to? :three }
-    it { should respond_to? :three? }
-    it { should respond_to? :three= }
-    it { should respond_to? :extra }
-    it { should_not respond_to? :three }
-    its(:two) { should == 'parent' }
+    context 'does not respond to three' do
+      it 'accessor' do
+        lambda { subject.three }.should raise_error NoMethodError
+      end
+      it 'predicate' do
+        lambda { subject.three? }.should raise_error NoMethodError
+      end
+      it 'setter' do
+        lambda { subject.three = 'THREE' }.should raise_error NoMethodError
+      end
+    end
+    its(:four)  { should == 'four' }
+    its(:extra) { should be_true }
+    its(:two)   { should == 'parent' }
   end
 end
